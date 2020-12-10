623973a5dfb9ddd9dfa47d94c7cc7bb41b886332
nagygabi <nagygabi1962@gmail.com>
2020-12-10 17:05:02 +0100

    ready

--- script.js
+++ script.js
@@ -1,24 +1,55 @@
-//tudu applikációt készítünk, alapértelmezetten 10 üres mező áll rendelkezésre, hozzá tudunk adni újakat;
-//checkbox s törlés gombbal
+//to do applikációt készítünk, alapértelmezetten 10 üres mező áll rendelkezésre, hozzá tudunk adni újakat;
+//checkbox és törlés gombb van mindegyik elemen, alatti egy gomb, amivel hozzáadnk új sorokat
 
-function pageLoaded(){
-    //változó benne root
+function pageLoaded() {
+    //változót hozok létre, amiben benne a root
+
     let root = document.getElementById("root");
-    //el kell tárolni max számot, itt 10
-    let maxNumber=10;
-    //struktúra kell több sorban
 
-    //duplikáljuk forral
+    //el kell tárolni a sorok hosszát, egy számot, itt 10
+    let maxNumber = 10;
 
-    //sor
+    //HTML-struktúra kell több sorban, legyen 1 div, 1 checkbox, input
+    let row =  ` 
+        <div class="row">
+            <input type="checkbox">
+            <input type="text">
+            <button class="delete">Delete</button>
+        </div>
+    `;
 
-    //kivülre: még egy sor hozzáadás
+    //for ciklussal 10-szer a sort kell létrehozni, insertAdjacebtHTML beforeend-del a html-hez hozzáadni a 10 sort
+    for (let index = 0; index < maxNumber; index++) {
+       root.insertAdjacentHTML("beforeend", row);
+    }
 
-    //klikk esemény
+    //a root elemen kivülre: egy plusz gomb hozzáadása, amivel kattintásra új sort tudunk hozzáadni
+    root.insertAdjacentHTML("afterend", `
+        <br><button id="add">Új sor hozzadása</button>
+    `);
+
+    //klikk eseményt adunk a plusz gombhoz, addevent click
+
+    function newItem(){
+        root.insertAdjacentHTML("beforeend", row);
+    }
+
+    document.getElementById("add").addEventListener("click", newItem);
 
     //kijelölöm classnév alapján az összes törlés gombot és elmentem egy változóba
 
+   let deleteButtons = root.querySelectorAll(".delete");
+
     //for ciklus: ráadni mindegyikre egy click eseményt, h mindegyiken történjen vmi a kattintás hatására --hogyan ...?
+    for (let index = 0; index < deleteButtons.length; index++){
+        deleteButtons[index].addEventListener("click", removeItem);
+    }
+
+    function removeItem(e){
+        console.log("e.target");
+        e.target.parentElement.remove();
+    }
 
 }
 window.addEventListener("load", pageLoaded);
+

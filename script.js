9cb8f901b9e0bb79b796616d4869ab416a48ea5f
nagygabi <nagygabi1962@gmail.com>
2020-12-10 09:30:41 +0100

    elso

---
+++ script.js
@@ -0,0 +1,24 @@
+//tudu applikációt készítünk, alapértelmezetten 10 üres mező áll rendelkezésre, hozzá tudunk adni újakat;
+//checkbox s törlés gombbal
+
+function pageLoaded(){
+    //változó benne root
+    let root = document.getElementById("root");
+    //el kell tárolni max számot, itt 10
+    let maxNumber=10;
+    //struktúra kell több sorban
+
+    //duplikáljuk forral
+
+    //sor
+
+    //kivülre: még egy sor hozzáadás
+
+    //klikk esemény
+
+    //kijelölöm classnév alapján az összes törlés gombot és elmentem egy változóba
+
+    //for ciklus: ráadni mindegyikre egy click eseményt, h mindegyiken történjen vmi a kattintás hatására --hogyan ...?
+
+}
+window.addEventListener("load", pageLoaded);
